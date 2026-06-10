182735fbbbb8336c50420cdbed66f52cb84e0153
Palanbyul <86470977+ssxst31@users.noreply.github.com>
2026-06-10 11:24:03 +0900

    refactor: switch purchase event tracking from GTM dataLayer to direct GA4 gtag implementation

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,29 +1,38 @@
-import TagManager from 'react-gtm-module'
+import TagManager from "react-gtm-module";
 
 export function init() {
-  TagManager.initialize({ gtmId: 'GTM-N9C9GPNW' })
+  TagManager.initialize({ gtmId: "GTM-N9C9GPNW" });
 }
 
 export function pageview(path) {
   TagManager.dataLayer({
-    dataLayer: { event: 'pageview', page: path },
-  })
+    dataLayer: { event: "pageview", page: path },
+  });
 }
 
-export function beginCheckout({ items, value, currency = 'KRW' }) {
+export function beginCheckout({ items, value, currency = "KRW" }) {
   TagManager.dataLayer({
-    dataLayer: { event: 'begin_checkout', currency, value, items },
-  })
+    dataLayer: { event: "begin_checkout", currency, value, items },
+  });
 }
 
-export function purchase({ transactionId, items, value, currency = 'KRW' }) {
-  TagManager.dataLayer({
-    dataLayer: {
-      event: 'purchase',
-      transaction_id: transactionId,
-      currency,
-      value,
-      items,
-    },
-  })
+export function purchase({ transactionId, items, value, currency = "KRW" }) {
+  // GTM으로도 전달
+  // TagManager.dataLayer({
+  //   dataLayer: {
+  //     event: 'purchase',
+  //     transaction_id: transactionId,
+  //     currency,
+  //     value,
+  //     items,
+  //   },
+  // })
+
+  // GA4로 직접 전달
+  window.gtag("event", "purchase", {
+    transaction_id: transactionId,
+    currency,
+    value,
+    items,
+  });
 }
